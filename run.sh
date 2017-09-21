7a182d64d69a674e55e8ade9df7267130bc5cf0e
zhfzhmsra@gmail.com <pb06210108>
2017-09-21 04:20:52 -0700

    s

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ git reset --hard
 make
 az storage blob download-batch -s https://zhfzhmsra.blob.core.windows.net/upload/ -d ./ --pattern all_titles_clean.txt
 
-#./fasttext nnSent models/all_titles_model.bin.bin all_titles_tokens.txt 3
+#./fasttext nnSent models/model.bin corpus.txt 3
 
-#./fasttext sent2vec -input all_titles_tokens.txt -output models/all_titles_model.bin -minCount 8 -dim 700 -epoch 9 -lr 0.2 -wordNgrams 2 -loss ns -neg 10 -thread 20 -t 0.000005 -dropoutK 4 -minCountLabel 20 -bucket 4000000
+#./fasttext sent2vec -input train_docs.txt -output models/model -minCount 8 -dim 100 -epoch 9 -lr 0.2 -wordNgrams 2 -loss ns -neg 10 -thread 4 -t 0.000005 -dropoutK 4 -minCountLabel 20 -bucket 10000
 
 
 
